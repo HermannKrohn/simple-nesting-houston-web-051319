283b9354362babaaf9d68f076ae8eae633205c86
Hermann Krohn <krohnhermannacosta@gmail.com>
2019-05-05 01:45:53 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -129,5 +129,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+    array = programmer_hash[:dennis_ritchie][:languages]
+    array.push("Assembly")
+    programmer_hash[:dennis_ritchie]
 end
